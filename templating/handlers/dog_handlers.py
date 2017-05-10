5d98ecd2a44b128cae0b56b0e3e4e3f3b64aa387
Vincent White <xstrengthofonex@gmail.com>
2017-05-11 00:24:27 +0900

    Fix EditDogHandler post method

--- templating/handlers/dog_handlers.py
+++ templating/handlers/dog_handlers.py
@@ -2,7 +2,7 @@ from handlers.base_handler import BaseHandler
 
 db = {
     1:{'id':1, 'name':'Fido', 'image_url':'https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg'},
-    2:{'id':2, 'name': 'cesar', 'image_url':'http://3.bp.blogspot.com/-NAJ179pS4VU/Up3IKdKSnVI/AAAAAAAAAvg/mGshsQ078Gk/s1600/bom-dia.jpg'}
+    2:{'id':2, 'name': 'Cesar', 'image_url':'http://3.bp.blogspot.com/-NAJ179pS4VU/Up3IKdKSnVI/AAAAAAAAAvg/mGshsQ078Gk/s1600/bom-dia.jpg'}
 }
 
 class ShowDogHandler(BaseHandler):
@@ -30,7 +30,7 @@ class EditDogHandler(BaseHandler):
         else:
             self.response.write("No dog found for id {}".format(id))
 
-    def post(self):
+    def post(self, id=None):
         name = self.request.params['name']
         url = self.request.params['image_url']
         self.response.write("Editing dog {}".format(name))
